301094376db0d93419af531c809c98bdc458b5c5
K.M.D.Priyashan <kmdpriyashanofficials2001@gmail.com>
2026-07-04 09:48:35 +0530

    Create the object with class and without class ✏️

--- Day06/app.js
+++ Day06/app.js
@@ -1,1 +1,23 @@
 console.log("Hello, World!");
+
+
+// mejem hadannath puluvan class ekak deela eken object eka coll karala object hadannath puluvan 
+class customer {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+let student1 = new customer("John Doe", 12);
+console.log(student1);
+
+// second method --> eken venne class ekak hadanna one na object ekak hadala eken value assign karanna puluvan 
+
+let student2 = {
+    name: "John Doe",
+     age: 12 
+    
+};
+
+console.log(student2);
