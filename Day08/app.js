4ad492190e78bab75b4061ec474129bb1551b3c6
K.M.D.Priyashan <kmdpriyashanofficials2001@gmail.com>
2026-07-25 09:25:19 -0400

    Arrays and object set to the local storage location. ✅

--- Day08/app.js
+++ Day08/app.js
@@ -11,7 +11,24 @@ localStorage.setItem("ids",{"id":"c001" , "id": "Coo2"})
 //items remoe laranne meken methana denne key eka thama 
 localStorage.removeItem("ids")
 
+
 //get karaganne mehema values
 let getName = localStorage.getItem("address")
 console.log(getName)
+
+//object ekak local storage ekata daganne ,ekemai
+let customer = {
+    name: "Dulash",
+    address: "Gampaha",
+    Age: 25
+}
+//meken thama string ekak vidiyata convert karaganne 
+let stringcustomer = JSON.stringify(customer);
+
+localStorage.setItem("Customers",stringcustomer);
+
+let getCustomer = localStorage.getItem("Customers");
+
+let jsoncustomer = JSON.parse(getCustomer);
+console.log(jsoncustomer);
 
